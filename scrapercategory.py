92a520624aea5e29605266c39b7ae4583ad7addd
AkaSan242 <Lucaka@icloud.com>
2021-05-18 15:15:20 +0200

    3eme version script qui recupere les urls de tout les livres sur une categorie

---
+++ scrapercategory.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+books_url = []
+#we'll use it to iterate on all category books links
+
+url = 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html'
+#category url
+
+next_path = url.replace('index.html', '')
+# use to create a path if there is a next page of result
+
+
+url_book_path = 'http://books.toscrape.com/catalogue/'
+#use to define the link of each book
+
+
+def get_books_links(url):
+
+    response = requests.get(url)
+    if response.ok:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        h = soup.find_all('h3')
+        next = soup.find('li', {'class': 'next'})
+
+        #to create the link of the book
+        for a in h:
+            for link in a:
+                links = link.get('href').replace('../../../', '')
+                books_url.append(url_book_path + links)
+
+
+        #if next page exist
+        if next:
+            for link in next.find_all('a'):
+                next_link = link.get('href')
+                next_page = next_path + next_link
+                get_books_links(next_page)
+
+
+
+
+get_books_links(url)
+
+#to check if we got all links
+print(len(books_url))
