48dfc2893868a515183f95d933fbea37e08e2879
AkaSan242 <Lucaka@icloud.com>
2021-05-18 14:42:53 +0200

    4eme version script pour scraper un livre

--- scraperbook.py
+++ scraperbook.py
@@ -3,40 +3,60 @@ from bs4 import BeautifulSoup
 import csv
 
 
-book_url = 'http://books.toscrape.com/catalogue/forever-and-forever-the-courtship-of-henry-longfellow-and-fanny-appleton_894/index.html'
-url = 'http://books.toscrape.com/' #use to get the image_url in the fonction (get_a_book)
+url = 'http://books.toscrape.com/catalogue/forever-and-forever-the-courtship-of-henry-longfellow-and-fanny-appleton_894/index.html'
+#book url
+
+url_i = 'http://books.toscrape.com/'
+#to define the path for the url of book image
+
+#create the headers of csvfile
+with open('onebook.csv', 'w', newline='', encoding='utf-8-sig') as books:
+        bookswriter = csv.writer(books, delimiter=',', dialect='excel')
+
+        bookswriter.writerow(
+        ['product_page_url', 'universal_product_code',
+         'title', 'price_including_tax'
+            , 'price_excluding_tax', 'number_available',
+         'product_description', 'category',
+         'review_rating', 'image_url'])
 
 
-def get_a_book(book_url):
+
+def get_a_book(url):
     """get book information"""
-    response = requests.get(book_url)
+    response = requests.get(url)
     if response.ok:
-        soup = BeautifulSoup(response.content, 'html.parser') #to read the doc and find the information
+        soup = BeautifulSoup(response.content, 'html.parser')
+        #to read the doc and find the information
+
+        #extract book informations
         td = soup.findAll('td')
         p = soup.findAll('p')
-
-        title = {'title': soup.h1.text}
-        product_description = {'description':p[3].text}
-        upc = {'upc': td[0].text}
-        price_excluding_tax = {'price(excl.tax)':td[2].text}
-        price_including_tax = {'price(incl.tax}':td[3].text}        #book information
-        number_available = {'available':td[5].text}
-        review_rating = {'review':td[6].text}
-        product_page_url = {'url':book_url}
-        image = soup.img['src'].replace('../../', '')
-        image_url = {'image url': url + image}
         a = soup.ul.find_all('a')
-        category = {'category':a[2].text}
+
+        #transform book informations
+        title = soup.h1.text
+        product_description = p[3].text.replace(',', '.')
+        upc = td[0].text
+        price_excluding_tax = td[2].text
+        price_including_tax = td[3].text
+        number_available = td[5].text
+        review_rating = td[6].text
+        product_page_url = url
+        image = soup.img['src'].replace('../../', '')
+        image_url = url_i + image
+        category = a[2].text
+
+        #load data on csv file
+        with open('onebook.csv', 'a', newline='', encoding='utf-8-sig') as books:
+            bookswriter = csv.writer(books, delimiter=',', dialect='excel')
+
+            bookswriter.writerow([product_page_url, upc,
+                                  title, price_including_tax,
+                                  price_excluding_tax, number_available
+                                     , product_description, category
+                                     , review_rating, image_url])
 
 
-    with open('books.csv', 'w', newline='', encoding='utf-8-sig') as books:  #write the info on a csvfile
 
-        fieldnames = ['product_page_url', 'universal_product_code', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
-        writer = csv.DictWriter(books, fieldnames=fieldnames, dialect='excel')
-        
-        writer.writeheader()
-        
-        writer.writerow({'product_page_url':product_page_url, 'universal_product_code':upc, 'title':title, 'price_including_tax':price_including_tax,'price_excluding_tax':price_excluding_tax,'number_available':number_available ,'product_description':product_description,'category':category,'review_rating':review_rating,'image_url':image_url})
-
-bookscraper = get_a_book(book_url)
-
+get_a_book(url)
