b2233de64188074db19bb4b3d48624e20c6bf382
AkaSan242 <Lucaka@icloud.com>
2021-05-20 14:39:46 +0200

    script pour scraper les liens des livres dans une categorie 5eme version

--- scrapercategory.py
+++ scrapercategory.py
@@ -2,25 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 
 
-books_url = []
+
+books_urls = []
 #we'll use it to iterate on all category books links
 
 url = 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html'
 #category url
 
-next_path = url.replace('index.html', '')
-# use to create a path if there is a next page of result
-
-
-url_book_path = 'http://books.toscrape.com/catalogue/'
-#use to define the link of each book
 
 
 def get_books_links(url):
 
+    url_book_path = 'http://books.toscrape.com/catalogue/'
+    # use to define the link of each book
+
     response = requests.get(url)
     if response.ok:
+
         soup = BeautifulSoup(response.content, 'html.parser')
+        books_number = soup.form.strong.text
+        #number of books in the category
+
+        category = soup.find('h1').text
         h = soup.find_all('h3')
         next = soup.find('li', {'class': 'next'})
 
@@ -28,20 +31,28 @@ def get_books_links(url):
         for a in h:
             for link in a:
                 links = link.get('href').replace('../../../', '')
-                books_url.append(url_book_path + links)
+                books_urls.append(url_book_path + links)
 
 
         #if next page exist
         if next:
+
             for link in next.find_all('a'):
                 next_link = link.get('href')
-                next_page = next_path + next_link
-                get_books_links(next_page)
+                next_path = 'http://books.toscrape.com/catalogue/category/books/{}_{}/'.format(category.lower().replace(' ', '-'), str(5))
+                # use to create a path if there is a next page of result                                                         #page index
 
+                next_page = next_path + next_link
+                #create next page url
+                get_books_links(next_page)
+        else:
+            print('il y a "{}" livres dans la catégorie "{}".'.format(books_number, category))
 
 
 
 get_books_links(url)
 
-#to check if we got all links
-print(len(books_url))
+
+#to compare with books numbers and check if we got all links
+print('il y a "{}" livres dans books urls.'.format(len(books_urls)))
+
