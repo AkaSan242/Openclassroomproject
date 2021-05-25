a119160538cd79e0b6c48e1f26b8f822c4bcc069
AkaSan242 <Lucaka@icloud.com>
2021-05-25 21:33:37 +0200

    Bookscrap application (one book version)

---
+++ BookscrapOnebook/scraperbook.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+from getbook import get_a_book
+
+
+url = 'http://books.toscrape.com/catalogue/forever-and-forever-the-courtship-of-henry-longfellow-and-fanny-appleton_894/index.html'
+#book url
+
+
+response = requests.get(url)
+if response.ok:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # to read the doc and find the information
+
+    a = soup.ul.find_all('a')
+    category = a[2].text
+
+
+    #create the headers of csvfile
+    with open('csvfiles/{}.csv'.format(category), 'w', newline='', encoding='utf-8-sig') as books:
+        bookswriter = csv.writer(books, delimiter=',', dialect='excel')
+
+        bookswriter.writerow(
+        ['product_page_url', 'universal_product_code',
+         'title', 'price_including_tax'
+            , 'price_excluding_tax', 'number_available',
+         'product_description', 'category',
+         'review_rating', 'image_url'])
+
+
+get_a_book(url)
